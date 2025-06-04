34188f74fb95e9a523521dad6615aea63b543320
Dzikri Dhaniawan <dhaniawan.dzikri@gmail.com>
2025-06-04 11:20:29 +0800

    1st commit

---
+++ app/src/main/java/com/example/apklayarnusantara/SplashActivity.kt
@@ -0,0 +1,22 @@
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.apklayarnusantara.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val btnMulai = findViewById<Button>(R.id.btnMulai)
+        btnMulai.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+    }
+}
+
+class LoginActivity {
+
+}
