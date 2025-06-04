c14c8069adaaa77f748739698a1041f66f29b00a
Dzikri Dhaniawan <dhaniawan.dzikri@gmail.com>
2025-06-04 19:48:26 +0800

    add register layout

--- app/src/main/java/com/example/apklayarnusantara/RegisterActivity.kt
+++ app/src/main/java/com/example/apklayarnusantara/RegisterActivity.kt
@@ -1,2 +1,17 @@
 package com.example.apklayarnusantara
 
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val signInText = findViewById<TextView>(R.id.txtSignin)
+        signInText.setOnClickListener {
+            finish()
+        }
+    }
+}
